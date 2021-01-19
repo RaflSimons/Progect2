59319e5d098d595011bdeaddd0e48949a3097a59
payl <raflsimons228@mail.ru>
2021-01-19 21:48:03 +0300

    add two section: catalog ,carusel

---
+++ src/js/control.js
@@ -0,0 +1,7 @@
+const tabs = document.querySelector('.catalog__tabs');
+const tab = document.querySelector('.catalog__tab');
+
+document.querySelector('.catalog__tab').addEventListener('click', function(){
+    tab.style.background = "red";
+});
+
